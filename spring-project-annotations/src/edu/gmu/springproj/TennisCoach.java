2706f1d53f1205660eb58d1e7d7ae277818ee278
Changhoon Ji <cji2@gmu.edu>
2020-01-07 17:28:03 -0500

    modify TennisCoach for constructor with dependency injection-@Autowired

--- spring-project-annotations/src/edu/gmu/springproj/TennisCoach.java
+++ spring-project-annotations/src/edu/gmu/springproj/TennisCoach.java
@@ -1,17 +1,33 @@
 package edu.gmu.springproj;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 	
+	private FortuneService fortuneService;
+	
 	public TennisCoach() {
 		System.out.println("TennisCoach: in no-arg constructor!");
+	}
+	
+	@Autowired
+	public TennisCoach(FortuneService aFortuneService) {
+		System.out.println("TennisCoach: in an arg constructor!");
+		
+		fortuneService = aFortuneService;
 	}
 
 	@Override
 	public String getDailyWorkout() {
 
 		return "Practice your backhand volley!";
+	}
+	
+	@Override
+	public String getDailyFortune() {
+
+		return fortuneService.getFortune();
 	}
 }

3164d3482d4092456795f96bb235ec5c852b9fc7
Changhoon Ji <cji2@gmu.edu>
2020-01-08 12:10:46 -0500

    create RandomFortuneService.java to test @Qualifiers

---
+++ spring-project-annotations/src/edu/gmu/springproj/RandomFortuneService.java
@@ -0,0 +1,30 @@
+package edu.gmu.springproj;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+	
+	// create an array of strings.
+	private String[] data = {
+			"The fear of the LORD is the beginning of knowledge",
+			"Listen to your father's instruction",
+			"If sinners entice you, do not give in to them"
+	};
+	
+	// create a random number generator.
+	private Random myRandom = new Random();
+	
+	@Override
+	public String getFortune() {
+		
+		// pick a random string from the array.
+		int index = myRandom.nextInt(data.length);
+		
+		String theFortune = data[index];
+		
+		return theFortune;
+	}
+}

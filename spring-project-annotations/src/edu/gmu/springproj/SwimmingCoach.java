a2fc679768188c2389afabd9077a0093f8d01bd4
Changhoon Ji <cji2@gmu.edu>
2020-01-09 13:31:06 -0500

    modify SwimmingCoach to comment its @Component annotation.

--- spring-project-annotations/src/edu/gmu/springproj/SwimmingCoach.java
+++ spring-project-annotations/src/edu/gmu/springproj/SwimmingCoach.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class SwimmingCoach implements Coach {
 	
 	@Autowired
